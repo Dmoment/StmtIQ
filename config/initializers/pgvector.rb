de2236ebdfe9f65fdbf0a0915f1d3b346b466b7f
Deepak Chauhan <rishudc119@gmail.com>
2026-01-12 17:36:53 +0530

    Fix pgvector initializer for Rails 8 compatibility

--- config/initializers/pgvector.rb
+++ config/initializers/pgvector.rb
@@ -1,16 +1,17 @@
 # frozen_string_literal: true
 
 # Register pgvector type with ActiveRecord
-# This removes the "unknown OID 335292" warning
+# This removes the "unknown OID" warning for vector types
+#
+# Note: Rails 8 changed type registration. Using the new approach.
 Rails.application.config.after_initialize do
   if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
-    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
-      def initialize_type_map(m = type_map)
-        super
-        # Register vector type (OID 335292) as a string type
-        # ActiveRecord will treat it as a string, but PostgreSQL handles it as vector
-        m.register_type 'vector', ActiveRecord::Type::String.new
-      end
+    # Register vector type using the modern Rails 8 approach
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:vector] = { name: 'vector' }
+
+    # Register the OID type mapping when a connection is established
+    ActiveSupport.on_load(:active_record) do
+      ActiveRecord::Type.register(:vector, ActiveRecord::Type::String, adapter: :postgresql)
     end
   end
 end
